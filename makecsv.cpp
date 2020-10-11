b90ed387a6ca4d89739b09c8636e189fe409394a
Benedictus Kent <benedictuskent123@gmail.com>
2020-10-11 20:08:43 +0800

    make big csv file

---
+++ makecsv.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<cstdlib>
+#include<string>
+#include<ctime>
+using namespace std;
+
+int main() 
+{
+    int FILE_SIZE;
+    string unit;
+    cout << "Input desired file size: ";
+    cin >> FILE_SIZE >> unit;
+    
+    transform(unit.begin(), unit.end(), unit.begin(), ::tolower);
+    if(unit.compare("gb") == 0){
+        FILE_SIZE *= 1000000000;
+    }
+    else if(unit.compare("gb") == 0){
+        FILE_SIZE *= 1000000;
+    }
+    else if(unit.compare("kb") == 0){
+        FILE_SIZE *= 1000;
+    }
+    else if(unit.compare("b") == 0){
+        ;
+    }
+
+    ofstream outfile;
+    outfile.open("one.csv");
+
+    const int MAX = 2147483647;
+    int counter, random, sign, size=0;
+    string text;
+
+    srand(time(0));
+
+    while(true)
+    {
+        counter = 0;
+        while(counter != 20)
+        {
+            // randomizer
+            random = rand() % MAX;
+	        sign = rand() % 2;
+            if(sign == 1)
+                random *= -1;
+
+            // append string
+            text.append(to_string(random));
+            if(counter < 19)
+                text.append("|");
+
+            // add counter
+            ++counter;
+        }
+
+        size += text.length();
+        if(size <= FILE_SIZE)
+        {
+            outfile << text << "\n";
+            text.clear();
+            size += 1;
+        }
+        else
+        {
+            outfile << text;
+            cout << size << endl;
+            break;
+        }
+    }
+        
+    outfile.close();
+
+    return 0;
+}
